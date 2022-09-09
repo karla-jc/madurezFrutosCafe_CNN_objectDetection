c70c456b41365f7fb2a0c24373541162d6b0760f
karla-jc <karlita10yacely@gmail.com>
2022-09-08 23:08:42 -0500

    app_movil

---
+++ Aplication_tflite/android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -0,0 +1,314 @@
+package org.tensorflow.lite.examples.detection;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.tensorflow.lite.examples.detection.customview.OverlayView;
+import org.tensorflow.lite.examples.detection.env.ImageUtils;
+import org.tensorflow.lite.examples.detection.env.Logger;
+import org.tensorflow.lite.examples.detection.env.Utils;
+import org.tensorflow.lite.examples.detection.tflite.Classifier;
+import org.tensorflow.lite.examples.detection.tflite.YoloV4Classifier;
+import org.tensorflow.lite.examples.detection.tracking.MultiBoxTracker;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnTomarFoto, btnSeleccionarImagen;
+
+    Uri imageUri;
+
+    int TOMAR_FOTO = 100;
+    int SELEC_IMAGEN = 200;
+
+    String CARPETA_RAIZ = "MisFotosApp";
+    String CARPETA_IMAGENES = "imagenes";
+    String RUTA_IMAGEN = CARPETA_RAIZ + CARPETA_IMAGENES;
+    String path;
+
+
+
+
+    public static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraButton = findViewById(R.id.cameraButton);
+        detectButton = findViewById(R.id.detectButton);
+        imageView = findViewById(R.id.imageView);
+        btnTomarFoto = findViewById(R.id.btnTomarFoto);
+        btnSeleccionarImagen = findViewById(R.id.btnSeleccionarImagen);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+
+        }
+        btnTomarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tomarFoto();
+            }
+        });
+
+        btnSeleccionarImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                seleccionarImagen();
+            }
+        });
+
+        cameraButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, DetectorActivity.class)));
+
+        detectButton.setOnClickListener(v -> {
+            Log.d("Imagen", "Img: "+imageView);
+
+                Handler handler = new Handler();
+
+                new Thread(() -> {
+                    final List<Classifier.Recognition> results = detector.recognizeImage(cropBitmap);
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            handleResult(cropBitmap, results);
+                        }
+                    });
+                }).start();
+
+
+        });
+
+
+        this.sourceBitmap = Utils.getBitmapFromAsset(MainActivity.this, "fondo.png");
+
+        this.cropBitmap = Utils.processBitmap(sourceBitmap, TF_OD_API_INPUT_SIZE);
+
+        this.imageView.setImageBitmap(cropBitmap);
+
+        initBox();
+    }
+
+    private static final Logger LOGGER = new Logger();
+
+    public static final int TF_OD_API_INPUT_SIZE = 416;
+
+    private static final boolean TF_OD_API_IS_QUANTIZED = false;
+
+    private static final String TF_OD_API_MODEL_FILE = "custom_yolov4_fp16.tflite";
+
+    private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/coco.txt";
+
+    // Minimum detection confidence to track a detection.
+    private static final boolean MAINTAIN_ASPECT = false;
+    private Integer sensorOrientation = 90;
+
+    private Classifier detector;
+
+    private Matrix frameToCropTransform;
+    private Matrix cropToFrameTransform;
+    private MultiBoxTracker tracker;
+    private OverlayView trackingOverlay;
+
+    protected int previewWidth = 0;
+    protected int previewHeight = 0;
+
+    private Bitmap sourceBitmap;
+    private Bitmap cropBitmap;
+    private Bitmap bitmap;
+    private Bitmap bitmapUri;
+
+    private Button cameraButton, detectButton;
+    private ImageView imageView;
+
+    private void initBox() {
+        previewHeight = TF_OD_API_INPUT_SIZE;
+        previewWidth = TF_OD_API_INPUT_SIZE;
+        frameToCropTransform =
+                ImageUtils.getTransformationMatrix(
+                        previewWidth, previewHeight,
+                        TF_OD_API_INPUT_SIZE, TF_OD_API_INPUT_SIZE,
+                        sensorOrientation, MAINTAIN_ASPECT);
+
+        cropToFrameTransform = new Matrix();
+        frameToCropTransform.invert(cropToFrameTransform);
+
+        tracker = new MultiBoxTracker(this);
+        trackingOverlay = findViewById(R.id.tracking_overlay);
+        trackingOverlay.addCallback(
+                canvas -> tracker.draw(canvas));
+
+        tracker.setFrameConfiguration(TF_OD_API_INPUT_SIZE, TF_OD_API_INPUT_SIZE, sensorOrientation);
+
+        try {
+            detector =
+                    YoloV4Classifier.create(
+                            getAssets(),
+                            TF_OD_API_MODEL_FILE,
+                            TF_OD_API_LABELS_FILE,
+                            TF_OD_API_IS_QUANTIZED);
+
+        } catch (final IOException e) {
+            e.printStackTrace();
+            LOGGER.e(e, "Exception initializing classifier!");
+            Toast toast =
+                    Toast.makeText(
+                            getApplicationContext(), "Classifier could not be initialized", Toast.LENGTH_SHORT);
+            toast.show();
+            finish();
+        }
+    }
+
+    private void handleResult(Bitmap bitmap, List<Classifier.Recognition> results) {
+        final Canvas canvas = new Canvas(bitmap);
+        final Paint paint = new Paint();
+        final Paint paint1 =new Paint();
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(2.0f);
+        paint1.setTextSize(14);
+        paint1.setColor(Color.BLUE);
+
+
+        final List<Classifier.Recognition> mappedRecognitions =
+                new LinkedList<Classifier.Recognition>();
+
+        for (final Classifier.Recognition result : results) {
+            final RectF location = result.getLocation();
+            final int detectedClass = result.getDetectedClass(); //yo
+            final String getTitle = result.getTitle();//yo
+            final String id = result.getId();
+            if (location != null && result.getConfidence() >= MINIMUM_CONFIDENCE_TF_OD_API) {
+                canvas.drawRect(location, paint);
+                canvas.drawText(getTitle, location.left, location.top, paint1);
+                location.describeContents();
+
+                Log.d("Clase:","ID: "+ detectedClass + " Titulo: "+ getTitle + " Coordenadas: "+location);
+//                cropToFrameTransform.mapRect(location);
+//
+//                result.setLocation(location);
+//                mappedRecognitions.add(result);
+            }else {
+                Toast.makeText(MainActivity.this,"No se ha dectado frutos", Toast.LENGTH_SHORT).show();
+            }
+        }
+//        tracker.trackResults(mappedRecognitions, new Random().nextInt());
+//        trackingOverlay.postInvalidate();
+        imageView.setImageBitmap(bitmap);
+    }
+
+    public void tomarFoto() {
+        String nombreImagen = "";
+        File fileImagen = new File(Environment.getExternalStorageDirectory(), RUTA_IMAGEN);
+        boolean isCreada = fileImagen.exists();
+
+        if (isCreada==false) {
+            isCreada = fileImagen.mkdirs();
+        }
+
+        if (isCreada==true) {
+            nombreImagen = (System.currentTimeMillis()/1000)+".jpg";
+        }
+
+        path = Environment.getExternalStorageDirectory()+File.separator+RUTA_IMAGEN+File.separator+nombreImagen;
+        File imagen = new File(path);
+
+        Intent intent = null;
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            String authorities = this.getPackageName()+".provider";
+            Uri imageUri = FileProvider.getUriForFile(this, authorities, imagen);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        }else {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
+        }
+
+        startActivityForResult(intent, TOMAR_FOTO);
+    }
+
+    public void seleccionarImagen() {
+        Intent galeria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(galeria, SELEC_IMAGEN);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == SELEC_IMAGEN){
+            imageUri = data.getData();
+            //imageView.setImageURI(imageUri);
+            try {
+                bitmapUri = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                Log.d("StoragePrediction", "Output_uri: "+imageUri);
+                this.cropBitmap = Utils.processBitmap(bitmapUri, TF_OD_API_INPUT_SIZE);
+                this.imageView.setImageBitmap(cropBitmap);
+                initBox();
+            } catch (IOException e) {
+                e.printStackTrace();
+                LOGGER.e(e, "Nada :c");
+            }
+
+            //this.sourceBitmap = Utils.getBitmapFromAsset(MainActivity.this, imageUri);
+
+
+        }else if(resultCode == RESULT_OK && requestCode == TOMAR_FOTO){
+            MediaScannerConnection.scanFile(MainActivity.this, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                @Override
+                public void onScanCompleted(String s, Uri uri) {
+
+                }
+            });
+            this.bitmap = BitmapFactory.decodeFile(path);
+            //this.sourceBitmap = Utils.getBitmapFromAsset(MainActivity.this, null);
+            //imageView.setImageBitmap(bitmap);
+            this.cropBitmap = Utils.processBitmap(bitmap, TF_OD_API_INPUT_SIZE);
+            this.imageView.setImageBitmap(cropBitmap);
+
+            initBox();
+
+
+
+
+
+
+        }
+
+    }
+}
